5ff4b15b65eadfe6040f53ccc207e730ca093599
Kaviya Sri Rajkumar <kaviyasri14@gmail.com>
2018-07-23 09:55:00 +0530

    unit testing basics

---
+++ cricket-problem/main.ts
@@ -0,0 +1,13 @@
+import { Batsman } from './batsman';
+import { Scorer } from './scorer';
+const scorer = new Scorer();
+const batsman1 = new Batsman('K L Rahul');
+const batsman2 = new Batsman('Rohit Sharma');
+scorer.addBatsman(batsman1);
+scorer.addBatsman(batsman2);
+scorer.calculateScore([1, 2, 0, 0, 4, 1, 6, 2, 1, 3]);
+scorer.printScore();
+
+// Here I need to update the para element in the dom
+
+document.getElementById('totalScore').innerHTML = scorer.totalScore + '';
